90fc63f96070c49f230b639bb59068ef7367a97a
Jingsheng Chen <jason2594746793@gmail.com>
2020-07-26 14:58:51 +0100

    Added win 10 notification

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from egsapi_transform import api_to_GasPrice
+from win10toast import ToastNotifier
 import time
 
-user_low = input("Price for low:")
+toast = ToastNotifier()
+
+user_low = input("Price for low: ")
 if not user_low:
-    user_low = 0
+    user_low = 9999
 else:
     try:
         user_low = float(user_low)
@@ -11,27 +14,57 @@ else:
         print("ValueError")
         exit(1)
 
+user_medium = input("Price for standard: ")
+if not user_medium:
+    user_medium = 9999
+else:
+    try:
+        user_medium = float(user_medium)
+    except ValueError:
+        print("ValueError")
+        exit(1)
+
+user_high = input("Price for fast: ")
+if not user_high:
+    user_high = 9999
+else:
+    try:
+        user_high = float(user_high)
+    except ValueError:
+        print("ValueError")
+        exit(1)
 
 
-# user_medium = input("Price for standard:")
-# if not user_medium:
-#     user_medium = None
-
-# user_high = input("Price for fast:")
-# if not user_high:
-#     user_high = None
 def price_compare(target, current):
-    if target < current:
-        print("No")
-    else:
-        print("Yes")
+    for x in range(3):
+        if target[x] > current[x]:
+            return True
+
+def Notification_head(target, current, reminder):
+    reminder = [False, False, False]
+    for x in range(3):
+        if target[x] > current[x]:
+            reminder[x] = True
+    print(reminder)
+    return reminder       
+
+def notificationer(target, current, reminder):
+    toast.show_toast("Your Target is reached ", "Low option " + str(reminder[0]) + "  Medium option " + str(reminder[1]) + "  High option " + str(reminder[2]) + "       Current price is " + str(current) + " Your target price is " + str(target) ,duration=1000, icon_path="coin.ico")
+
 
 while True:
-    time.sleep(15)
     current_price = api_to_GasPrice()
-    print("current price:" + str(current_price))
-    print("User price:" + str(user_low))
-    price_compare(user_low, current_price.low)
+    show_current_price = str(current_price)
+    show_target_price = "low: " + str(user_low) + "  medium: " + str(user_medium) + "  high: "  + str(user_high)
+    print("current price:" + show_current_price)
+    print("User price:   " + show_target_price)
+    target_list = [user_low, user_medium, user_high]
+    current_list = [current_price.low, current_price.medium, current_price.high]
+    if price_compare(target_list, current_list):
+        reminder = []
+        reminder = Notification_head(target_list, current_list, reminder)
+        notificationer(show_target_price, show_current_price, reminder)
+    time.sleep(15)
 
 
         
